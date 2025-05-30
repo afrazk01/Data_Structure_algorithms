5b8de7b150ac2fd24eca947910d54dc8adfe247a
Afraz Khan <afrazkhan0714@gmail.com>
2025-05-30 23:52:32 +0500

    more implementations of OOP

--- OOP/more.py
+++ OOP/more.py
@@ -1,16 +1,32 @@
 class Person:
-    def __init__(self,name,age):
-        self.name = ''
-        self.age = 0
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
         self.__balance = 0
 
-    def set_balance(self,balance):
-        self.__balance = balance
+    def __str__(self):
+        return str(self.get_balance())
 
-    def get_balance(self):
-        balance = self.__balance
-        return balance
+    def set_balance(self, balance):
+        self.__balance = balance
     
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+    
+    def set_age(self, age):
+        self.__age = age
+
+    def get_age(self):
+        return self.__age
+    
+    def get_balance(self):
+        return self.__balance
+
 if __name__ == "__main__":
-    p = Person("afraz",24)
-    print(p.get_balance  )  
+    p = Person("afraz", 24)
+    print(p.get_balance()) 
+    p.set_balance(100)
+    print(p) 
