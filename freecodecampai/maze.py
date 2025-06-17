95427d77db5001f79263cd4132d47ba4717baf0f
Afraz Khan <afrazkhan0714@gmail.com>
2025-06-17 14:31:01 +0500

    freecodecamp ai search

---
+++ freecodecampai/maze.py
@@ -0,0 +1,21 @@
+import sys
+# classes and code for maze
+class Node:
+    def __init__(self,state,parent,action):
+        self.state = state 
+        self.parent = parent 
+        self.action = action
+
+class StackFrontier:
+    def __init__(self):
+        self.frontier = []
+
+    def add(self,node):
+        self.frontier.append(node)
+
+    def contain_state(self,state):
+        return any(node.state == state for node in self.frontier)
+    
+    def empty(self):
+        return len(self.frontier) == 0
+        
