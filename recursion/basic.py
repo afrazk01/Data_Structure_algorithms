48698f33d6d9cb9847d22866e99e0d8d9fcc3ed5
Afraz Khan <afrazkhan0714@gmail.com>
2025-07-07 22:03:03 +0500

    recursion added

---
+++ recursion/basic.py
@@ -0,0 +1,22 @@
+# function calling itself by a condition once reached condition it is completed is recursion two types head and tail
+# head recursion (print(hello world four times))
+
+
+def head_rec():
+    global count
+    if count == 4:
+        return
+    print("hello world")
+    count += 1
+    head_rec()
+
+def tail_rec():
+    global count
+    if count == 4:
+        return
+    count +=1
+    tail_rec()
+    print("Hello World")
+if __name__ == "__main__":
+    count = 0
+    tail_rec()
